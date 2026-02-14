bcefd55fb3f46b7cfaa00424e15df86306161105
Kimki999 <chutikan_chom@cmu.ac.th>
2026-02-14 15:09:45 +0700

    add category variable in ingredient.dart and mock_user_ingredients.dart

--- lib/core/mock/mock_user_ingredients.dart
+++ lib/core/mock/mock_user_ingredients.dart
@@ -3,24 +3,28 @@ import '../models/ingredient.dart';
 final List<Ingredient> mockUserIngredients = [
   Ingredient(
     name: 'Egg',
+    category: "Protein",
     addedDate: DateTime.now().subtract(const Duration(days: 2)),
     expiryDate: DateTime.now().add(const Duration(days: 5)),
     inFridge: true,
   ),
   Ingredient(
     name: 'Milk',
+    category: "Protein",
     addedDate: DateTime.now().subtract(const Duration(days: 4)),
     expiryDate: DateTime.now().add(const Duration(days: 2)),
     inFridge: true,
   ),
   Ingredient(
     name: 'Onion',
+    category: "Vegetables",
     addedDate: DateTime.now().subtract(const Duration(days: 7)),
     expiryDate: DateTime.now().add(const Duration(days: 10)),
     inFridge: true,
   ),
   Ingredient(
     name: 'Garlic',
+    category: "Vegetables",
     addedDate: DateTime.now().subtract(const Duration(days: 3)),
     expiryDate: DateTime.now().add(const Duration(days: 20)),
     inFridge: true,
@@ -29,6 +33,7 @@ final List<Ingredient> mockUserIngredients = [
   /// ❌ อันนี้ไม่อยู่ในตู้เย็น → จะไม่ถูก match
   Ingredient(
     name: 'Flour',
+    category: "Carbohydrate",
     addedDate: DateTime.now().subtract(const Duration(days: 20)),
     expiryDate: DateTime.now().add(const Duration(days: 200)),
     inFridge: false,
--- lib/core/models/ingredient.dart
+++ lib/core/models/ingredient.dart
@@ -1,11 +1,13 @@
 class Ingredient {
   final String name;
+  final String category;
   final DateTime addedDate;
   final DateTime expiryDate;
-  final bool inFridge;
+  final bool inFridge; 
 
   Ingredient({
     required this.name,
+    required this.category,
     required this.addedDate,
     required this.expiryDate,
     this.inFridge = false,
